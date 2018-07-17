bae4a45f4f1fbeb713ae2022db11b624efbd2d31
George <halin.george@gmail.com>
2018-07-17 23:03:37 +0300

    12313213

--- shared-pointer.cpp
+++ shared-pointer.cpp
@@ -1,77 +1,50 @@
 #include <iostream>
 
-class RefCounter {
-public:
-    RefCounter() : counter(nullptr) {}
-    RefCounter(int counter = 0) : counter(new int(counter)) {}
-
-    void swap(RefCounter &t) {
-        std::swap(counter, t.counter);
-    }
-
-    void AddRef() {
-        ++(*counter);
-    }
-
-    void ReleaseRef() {
-        --(*counter);
-    }
-
-    int Get() const {
-        return *counter;
-    }
-
-    ~RefCounter() {
-        if (counter != nullptr) {
-            delete counter;
-        }
-    }
-
-private:
-    int *counter;
-};
-
 template <class T>
 class shared_ptr {
 public:
-    explicit shared_ptr() : ptr(nullptr), counter(RefCounter(0)) {}
+    explicit shared_ptr() : ptr(nullptr), counter(nullptr) {}
 
-    explicit shared_ptr(T *t) : ptr(t), counter(RefCounter(1)) {
+    explicit shared_ptr(T *t) : ptr(t), counter(new int(1)) {
         std::cout << "constructor\n";
     }
 
     void swap(shared_ptr &t) {
         std::swap(ptr, t.ptr);
-        counter.swap(t.counter);
+        std::swap(counter, t.counter);
     }
 
     shared_ptr(const shared_ptr &t) {
         std::cout << "copy constructor\n";
         ptr = t.ptr;
         counter = t.counter;
-        counter.AddRef();
+        ++(*counter);
     }
 
     shared_ptr(shared_ptr &&t) : ptr(std::move(t.ptr)), counter(std::move(t.counter)) {
         std::cout << "move constructor\n";
     }
 
-    shared_ptr& operator=(shared_ptr t)  {
+    shared_ptr& operator=(shared_ptr &&t)  {
         std::cout << "operator=\n";
         swap(t);
     }
 
     ~shared_ptr() {
-        counter.ReleaseRef();
-        if (!counter.Get()) {
-            if (ptr != nullptr) {
-                delete ptr;
+        std::cout << "~shared_ptr()\n";
+        if (counter != nullptr) {
+            --(*counter);
+            if (!(*counter)) {
+                if (ptr != nullptr) {
+                    delete ptr;
+                }
+                delete counter;
             }
         }
     }
 
     int use_count() const {
-        return counter.Get();
+        return *counter;
     }
 
     T* operator->() {
@@ -84,7 +57,7 @@ public:
 
 private:
     T *ptr;
-    RefCounter counter;
+    int *counter;
 };
 
 int main() {
